eaf5b721978983f4bf6f8022f33bc21ab9e28f95
Marti <shirogg10@gmail.com>
2025-12-05 10:09:50 +0100

    fix

--- extensions.py
+++ extensions.py
@@ -1,14 +1,14 @@
 import os
 
-
 async def load_extensions(bot):
 
     for file in os.listdir("./commands"):
         if file.endswith(".py") and not file.startswith("__"):
             module_name = os.path.splitext(file)[0]
+
+            if module_name == "daily_task":
+                continue
+
             module = f"commands.{module_name}"
             print("Loading extension:", module)
             await bot.load_extension(module)
-
-    from tasks import setup_tasks
-    setup_tasks(bot)
